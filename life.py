99e29c4a2bd41cd5fef07ed7ea40ae79a6027500
Tom McDermott <spon@wattwatchers.com.au>
2018-10-06 08:49:35 +1000

    stop/go, speed

--- life.py
+++ life.py
@@ -1,54 +1,75 @@
 import pyxel
-import random
 import time
-import math
 
 
 class App:
-    MAX_R = 10
-
-    def new_circle(self):
-        self.start = time.time()
-        self.r = 0
-        self.x = random.randint(self.MAX_R, pyxel.width - self.MAX_R)
-        self.y = random.randint(self.MAX_R, pyxel.height - self.MAX_R)
-
-    def is_in_circle(self, xx, yy):
-        d = math.hypot(xx - self.x, yy - self.y)
-        return d <= self.r
-
     def __init__(self):
-        pyxel.init(160, 120, border_width=10, border_color=3)
-        self.speed = 1
-        self.score = 0
-        self.lives = 3
-        self.new_circle()
+        pyxel.init(16, 12, caption="Conway's Life", scale=50)
+        self.running = False
+        # grid[x][y]
+        self.grid = [[0 for i in range(pyxel.height)] for j in range(pyxel.width)]
+        self.tick_period = 1.0
+        self.last_tick = time.time()
         pyxel.run(self.update, self.draw)
 
+    def interact(self):
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            self.running = not self.running
+        if pyxel.btnp(pyxel.KEY_LEFT_BUTTON):
+            self.grid[pyxel.mouse_x][pyxel.mouse_y] ^= 1
+        if pyxel.btnp(pyxel.KEY_LEFT):
+            self.tick_period *= 1.5
+        if pyxel.btnp(pyxel.KEY_RIGHT):
+            self.tick_period /= 1.5
+
+    def neighbour_count(self, x, y):
+        count = 0
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                xx = x + dx
+                yy = y + dy
+                if dx == 0 and dy == 0:
+                    continue
+                if xx < 0 or xx >= pyxel.width:
+                    continue
+                if yy < 0 or yy >= pyxel.height:
+                    continue
+                count += self.grid[xx][yy]
+        return count
+
     def update(self):
-        if pyxel.btn(pyxel.KEY_LEFT_BUTTON):
-            # print(pyxel.mouse_x, pyxel.mouse_y)
-            if self.is_in_circle(pyxel.mouse_x, pyxel.mouse_y):
-                self.score += (11 - self.r)
-                self.new_circle()
-                self.speed *= 0.9
-        if time.time() - self.start > self.speed:
-            self.r += 1
-            self.start = time.time()
-            if self.r >= self.MAX_R:
-                self.new_circle()
-                self.lives -= 1
+        self.interact()
+        if self.running and time.time() - self.last_tick > self.tick_period:
+            self.last_tick = time.time()
+            new_grid = [[0 for i in range(pyxel.height)] for j in range(pyxel.width)]
+            for x in range(pyxel.width):
+                for y in range(pyxel.height):
+                    n = self.neighbour_count(x, y)
+                    if self.grid[x][y] == 1 and n in (0, 1):
+                        new_grid[x][y] = 0
+                    if self.grid[x][y] == 1 and n in (2, 3):
+                        new_grid[x][y] = 1
+                    if self.grid[x][y] == 1 and n > 3:
+                        new_grid[x][y] = 0
+                    if self.grid[x][y] == 0 and n == 3:
+                        new_grid[x][y] = 1
+            self.grid = new_grid
 
     def draw(self):
-        pyxel.cls(0)
-        pyxel.text(0, 0, f"score {self.score:4d} speed {1/self.speed:.2f}", 3)
-        for i in range(self.lives):
-            pyxel.rect(120 + i * 5, 0, 122 + i * 5, 4, 8)
-        if self.lives >= 0:
-            if self.r > 0:
-                pyxel.circ(self.x, self.y, self.r, 9)
+        if self.running:
+            # running - red on black
+            bg = 0
+            fg = 8
         else:
-            pyxel.text(50, 50, "GAME OVER", pyxel.frame_count % 16)
+            # stopped - black on white
+            bg = 7
+            fg = 0
+            pyxel.cls(0)
+        pyxel.cls(bg)
+        for x in range(pyxel.width):
+            for y in range(pyxel.height):
+                if self.grid[x][y] == 1:
+                    pyxel.pix(x, y, fg)
 
 
 App()
