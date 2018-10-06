ec69f8eecd28732c6ddfc3e795845d3245317b38
Tom McDermott <spon@wattwatchers.com.au>
2018-10-06 12:02:32 +1000

    two colours, two clicks

--- circle.py
+++ circle.py
@@ -12,6 +12,7 @@ class App:
         self.r = 0
         self.x = random.randint(self.MAX_R, pyxel.width - self.MAX_R)
         self.y = random.randint(self.MAX_R, pyxel.height - self.MAX_R)
+        self.colour = 12
 
     def is_in_circle(self, xx, yy):
         d = math.hypot(xx - self.x, yy - self.y)
@@ -21,17 +22,20 @@ class App:
         pyxel.init(160, 120)
         self.speed = 1
         self.score = 0
-        self.lives = 3
+        self.lives = 10
         self.new_circle()
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        if pyxel.btn(pyxel.KEY_LEFT_BUTTON):
+        if pyxel.btnp(pyxel.KEY_LEFT_BUTTON):
             # print(pyxel.mouse_x, pyxel.mouse_y)
             if self.is_in_circle(pyxel.mouse_x, pyxel.mouse_y):
-                self.score += (11 - self.r)
-                self.new_circle()
-                self.speed *= 0.9
+                if self.colour == 12:
+                    self.colour = 9
+                else:
+                    self.score += (11 - self.r)
+                    self.new_circle()
+                    self.speed *= 0.9
         if time.time() - self.start > self.speed:
             self.r += 1
             self.start = time.time()
@@ -44,9 +48,9 @@ class App:
         pyxel.text(0, 0, f"score {self.score:4d} speed {1/self.speed:.2f}", 3)
         for i in range(self.lives):
             pyxel.rect(120 + i * 5, 0, 122 + i * 5, 4, 8)
-        if self.lives > 0:
+        if self.lives >= 0:
             if self.r > 0:
-                pyxel.circ(self.x, self.y, self.r, 9)
+                pyxel.circ(self.x, self.y, self.r, self.colour)
         else:
             pyxel.text(50, 50, "GAME OVER", pyxel.frame_count % 16)
 
